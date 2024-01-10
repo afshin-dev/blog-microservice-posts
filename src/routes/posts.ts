ae9b0f243287798a801e0a362e1673efbe954209
afshin <shahinof10009@gmail.com>
2024-01-10 16:04:30 +0330

    check tilte of new post to not be null or empty string

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -2,12 +2,20 @@ import { Router } from "express";
 import { selectPosts, addPost } from "../store/slices/posts.slice.js";
 import { store } from "../store/index.js";
 import { Post } from "../store/slices/posts.slice.js";
+import { STATUS_CODES } from "node:http";
+
 export const posts: Router = Router({});
 
 posts.get("", (req, res) => {
   res.json(selectPosts(store.getState()));
 });
 posts.post("", (req, res) => {
+  // title must be in request body
+  if (!req.body.title) {
+    res.status(400).json({ error: STATUS_CODES["400"] });
+    return;
+  }
+
   const post: Post = {
     title: req.body?.title,
     content: req.body?.content,
